2e004cc2052ee8b92644f34ce8b01c375befad5c
Gonzalo Melo <gonzalomelov@gmail.com>
2024-10-05 15:35:47 -0300

    chore: use the OpenAI Realtime API demo app starting from a vanilla Next project

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  webpack: (config) => {
+    config.externals.push({
+      'utf-8-validate': 'commonjs utf-8-validate',
+      'bufferutil': 'commonjs bufferutil',
+    })
+    return config
+  },
+}
 
 export default nextConfig;
