62c10beb5051df36a804a37233292be7c9289583
Gonzalo Melo <gonzalomelov@gmail.com>
2024-10-11 18:22:24 -0300

    chore: use Spanish

--- src/components/console/Console.tsx
+++ src/components/console/Console.tsx
@@ -242,7 +242,7 @@ export function Console() {
     client.sendUserMessageContent([
       {
         type: `input_text`,
-        text: `Hello!`,
+        text: `Hola!`,
         // text: `For testing purposes, I want you to list ten car brands. Number each item, e.g. "one (or whatever number you are one): the item name".`
       },
     ]);
