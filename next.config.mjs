e82a6b8dd09d482587f77fad0b417122ad8b3d5b
Gonzalo Melo <gonzalomelov@gmail.com>
2024-10-13 07:33:45 -0300

    chore: allow smart wallet on mobile

--- next.config.mjs
+++ next.config.mjs
@@ -8,6 +8,19 @@ const nextConfig = {
     })
     return config
   },
-}
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Cross-Origin-Opener-Policy',
+            value: 'same-origin-allow-popups',
+          },
+        ],
+      },
+    ];
+  },
+};
 
 export default nextConfig;
